da63f1c11d376279106e5842a0c28eec193da8ac
Sivet <simon@glashoff.dk>
2021-03-12 16:05:21 +0100

    Swagger wont launch with summaries

--- toejController/Controllers/ItemOverviewController.cs
+++ toejController/Controllers/ItemOverviewController.cs
@@ -7,12 +7,47 @@ using Microsoft.Extensions.Logging;
 
 namespace toejController.Controllers{
 
+    /// <summary>Controller responsible for GET/POST/DELETE for managing Item Catalog </summary>
     [ApiController]
-    [Route("[controller]")]
-    class ItemOverviewController : ControllerBase{
+    [Route("toej")]
+    public class ItemOverviewController : ControllerBase{
 
+        /// <summary>This GET method returns </summary>
+        /// <returns>An arrey of MakerSpases</returns>
         [HttpGet]
         public string GetImage(string imageName)
+        {
+            return "s";
+        }
+
+        /// <summary>This GET method returns all MakerSpaces from DB</summary>
+        /// <returns>An arrey of MakerSpases</returns>
+        [HttpGet]
+        public string GetClothType(string gender)
+        {
+            return "s";
+        }
+
+        /// <summary>This GET method returns all MakerSpaces from DB</summary>
+        /// <returns>An arrey of MakerSpases</returns>
+        [HttpGet]
+        public string GetAllofType(string type, string gender)
+        {
+            return "s";
+        }
+
+        /// <summary>This GET method returns all MakerSpaces from DB</summary>
+        /// <returns>An arrey of MakerSpases</returns>
+        [HttpGet]
+        public string GetInfoOnItem(string item)
+        {
+            return "s";
+        }
+
+        /// <summary>This GET method returns all MakerSpaces from DB</summary>
+        /// <returns>An arrey of MakerSpases</returns>
+        [HttpGet]
+        public string GetAllImgOnItem(string item)
         {
             return "s";
         }
--- toejController/Startup.cs
+++ toejController/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,7 +31,13 @@ namespace toejController
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "toejController", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "toejController", 
+                    Version = "v1" 
+                    });
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    c.IncludeXmlComments(xmlPath);
             });
         }
 
@@ -53,7 +61,6 @@ namespace toejController
             {
                 endpoints.MapControllers();
             });
-
 
             //Delete later when we gonna be ready to release!!!
             app.UseSwagger();
