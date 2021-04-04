7409eae63734e187079a89c1278dcad87bc260fe
Sivet <simon@glashoff.dk>
2021-04-04 12:34:44 +0200

    Added context to startup

--- StoreInventory/Startup.cs
+++ StoreInventory/Startup.cs
@@ -8,11 +8,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StoreInventory.Contexts;
 
 namespace StoreInventory
 {
@@ -28,7 +30,9 @@ namespace StoreInventory
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ClothesContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("NonsensConnection")));
+                
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
